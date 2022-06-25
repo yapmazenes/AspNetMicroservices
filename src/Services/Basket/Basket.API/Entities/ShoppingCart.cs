b894f71b4687d937a7df3c508550b44379cd27d3
yapmazenes <yapmazenes@gmail.com>
2022-06-26 00:16:26 +0300

    Added Basket.API, ShoppingCart, Shopping Cart Item, Repositoy Created

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+
+                return totalPrice;
+            }
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,43 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public BasketRepository(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
+        }
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            if (!string.IsNullOrEmpty(userName))
+            {
+                var basketCache = await _distributedCache.GetStringAsync(userName);
+
+                return JsonConvert.DeserializeObject<ShoppingCart>(basketCache);
+            }
+
+            return null;
+        }
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _distributedCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _distributedCache.RemoveAsync(userName);
+        }
+    }
+}
