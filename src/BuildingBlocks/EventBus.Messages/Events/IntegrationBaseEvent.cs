8ccbced35b1a9fc85d90c31ef0ca0eb2a330a63f
yapmazenes <yapmazenes@gmail.com>
2022-07-07 07:35:22 +0300

    BuildingBlocks Created and EventBus Common Classes Created, BasketCheckoutEvent Created, ,Added RabbitMq docker-compose, EventBus specified RabbitMq and Implemented Basket.API

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            CorrelationId = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(Guid correlationId, DateTime creationDate)
+        {
+            CorrelationId = correlationId;
+            CreationDate = creationDate;
+        }
+
+        public Guid CorrelationId { get; private set; }
+
+        public DateTime CreationDate { get; private set; }
+    }
+}
--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -28,14 +29,27 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Redis Configuration
             services.AddStackExchangeRedisCache(options =>
             {
                 options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
 
+            //General Configuration
             services.AddScoped<IBasketRepository, BasketRepository>();
+
+            //Grpc Configuration
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
             services.AddScoped<DiscountGrpcService>();
+
+            //MassTransit-RabbitMQ Configuration
+            services.AddMassTransit(config =>
+            {
+                config.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
